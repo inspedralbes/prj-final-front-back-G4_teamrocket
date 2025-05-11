b2efecb79cc5cf5bec82e773bec979c863a667f2
a18marcastru <a18marcastru@inspedralbes.cat>
2025-05-12 01:13:47 +0200

    Tabla Reactions en MongoDB.

---
+++ Back/MongoDB/routes/api-reactions.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import Reaction from '../models/reaction.js';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const reactions = Reaction.findAll();
+        res.json(reactions);
+    } catch (error) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.put('/like/:modId', async (req, res) => {
+    try {
+        const modId = req.params.modId;
+        
+        const todayDate = new Date();
+        todayDate.setHours(0,0,0,0);
+
+        await ModDailyReactions.findOneAndUpdate(
+            { modId, date: todayDate },
+            { $inc: { totalLikes: 1 } },
+            { upsert: true}
+        );
+    } catch {
+        console.error(error);
+        res.status(500).json({ error: 'Ocurrió un error al registrar el like.' });
+    }
+});
+
+router.put('/dislike/:modId', async (req, res) => {
+    try {
+        const modId = req.params.modId;
+
+        const todayDate = new Date();
+        todayDate.setHours(0, 0, 0, 0);
+
+        await ModDailyReactions.findOneAndUpdate(
+            { modId, date: todayDate },
+            { $inc: { totalDislikes: 1 } },
+            { upsert: true, new: true }
+        );
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Ocurrió un error al registrar el dislike.' });
+    }
+});
